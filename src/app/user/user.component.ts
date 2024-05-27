b4ef7fe9912010e1fd386606b2478179921945cb
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-27 21:23:06 +0200

    add tooltip

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIcon],
+  imports: [MatButtonModule, MatIcon, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })

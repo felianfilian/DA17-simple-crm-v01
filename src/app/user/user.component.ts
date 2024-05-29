803d450e3076c3b95006735bfb8f5a4c0982c37f
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-29 10:09:14 +0200

    user dialog test

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -17,13 +17,5 @@ export class UserComponent {
   animal = 'yoshi';
 
   openDialog() {
-    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: { name: this.name, animal: this.animal },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
+    this.dialog.open(UserComponent);
 }

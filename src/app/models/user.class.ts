1ba08a17771667eaa17efc51bd8e56eee7b8e61d
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-31 23:57:26 +0200

    create more class elements

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -1,5 +1,12 @@
 export class User {
   firstName!: string;
   lastName!: string;
-  birthDate!: string;
+  birthDate!: number;
+  street!: string;
+  zipcode!: number;
+  city!: string;
+
+  constructor(obj?: any) {
+    this.firstName = obj.firstName;
+  }
 }

2772dcbf4cf3b4f40a1f667295e0947ad0d8291e
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-01 00:01:38 +0200

    user constructor

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -7,6 +7,11 @@ export class User {
   city!: string;
 
   constructor(obj?: any) {
-    this.firstName = obj.firstName;
+    this.firstName = obj ? obj.firstName : 'Mario';
+    this.lastName = obj ? obj.lastName : 'Super';
+    this.birthDate = obj ? obj.birthDate : '13.01.1984';
+    this.street = obj ? obj.street : 'Mushroom Street';
+    this.zipcode = obj ? obj.zipcode : '12345';
+    this.city = obj ? obj.city : 'Toad Land';
   }
 }
